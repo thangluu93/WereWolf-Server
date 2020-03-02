95197a2e117051a99c9bde5f35139037d23b9361
leinailychee <leinailychee__@users.noreply.github.com>
2020-03-02 12:14:57 +0700

    create main server.js

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const index=  require('./index').creatNewServer;
-const server=new index()
-
-// io.on('connection', socket => {
-//   console.log('-------' + socket.id + '--------')
-//   socket.emit('my broadcast',() {
-//     message: 'Welcome to WereWolf-itss.'
-//   })
-//   socket.broadcast.emit('my broadcast', {
-//     message: 'A new client has connected.'
-//   })
-
-//   socket.on('send', function (data) {
-//     console.log(data)
-//     io.emit('my broadcast', data)
-//   })
-// })
-
-
+const index = require('./index').creatNewServer;
+const server = new index()
 
 app.get('/', (req, res) => {
     res.send('<h1>Hey Socket.io</h1>')
-  })
-  
-  
-  
-  http.listen(3000, () => {
+})
+
+http.listen(3000, () => {
     console.log('listening on *:3000')
+})
+
+
+
+
+io.on('connection', socket => {
+  console.log('-------' + socket.id + '--------')
+  socket.emit('my broadcast',() {
+    message: 'Welcome to WereWolf-itss.'
   })
-  
-  
+  socket.broadcast.emit('my broadcast', {
+    message: 'A new client has connected.'
+  })
+
+  socket.on('send', function (data) {
+    console.log(data)
+    io.emit('my broadcast', data)
+  })
+})
+
+
+
+
+
